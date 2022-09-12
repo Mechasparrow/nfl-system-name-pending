0eb64aa61f2ed5db560ef3b6852ce0e7ee5b5b93
Michael Navazhylau <mikipux7@gmail.com>
2022-09-12 12:32:25 -0500

    refactor matchup parser functions

--- matchup_parser.py
+++ matchup_parser.py
@@ -21,37 +21,54 @@ def get_nfl_soup(year, week):
     }
 
     nfl_json_api_url = f'{json_template_str}{json.dumps(nfl_json)}'
-
     nfl_json_response = requests.get(nfl_json_api_url)
+
     return BeautifulSoup(nfl_json_response.text, 'html.parser')
 
-def get_nfl_matchups(year, nfl_json_soup):
 
+
+def find_team_name(team_soup):
+    return team_soup.find(class_="nfl-c-matchup-strip__team-name").find(class_="nfl-c-matchup-strip__team-fullname").text.strip()
+
+def get_matchup_teams(matchup_soup):
+    matchup_teams_soup = matchup_soup.find(class_ = "nfl-c-matchup-strip__game").find_all(class_ = "nfl-c-matchup-strip__team")
+    matchup_team_names = list(map(lambda name: find_team_name(name), matchup_teams_soup))
+    return (matchup_team_names[0], matchup_team_names[1])
+
+def get_matchup_time(matchup_soup):
+    matchup_time_details = matchup_soup.find(class_ = "nfl-c-matchup-strip__date-time")
+    matchup_time = None
+    if (matchup_time_details != None):
+        matchup_time = matchup_time_details.text.strip()
+
+    return matchup_time 
+
+def get_nfl_section_information(nfl_json_soup):
     nfl_json_sections = nfl_json_soup.find_all(class_ = "nfl-o-matchup-group")
-
-    nfl_matchup_list = []
+    
+    section_information_list = []
 
     for section in nfl_json_sections:
-        section_title = section.find(class_ = "d3-o-section-title")
-        section_date = section_title.text
+        section_date = section.find(class_ = "d3-o-section-title").text
         matchups = section.find_all(class_ = "nfl-c-matchup-strip")
-        
+
+        section_detail = (section_date, matchups)
+        section_information_list.append(section_detail)
+
+    return section_information_list
+
+def get_nfl_matchups(year, nfl_json_soup):
+    nfl_matchup_list = []
+
+    nfl_sections = get_nfl_section_information(nfl_json_soup)
+
+    for (section_date, matchups) in nfl_sections:
         for matchup in matchups:
-            matchup_details = matchup.find(class_ = "nfl-c-matchup-strip__game")
-            matchup_teams_soup = matchup_details.find_all(class_ = "nfl-c-matchup-strip__team")
-            find_team_name = lambda team_soup: team_soup.find(class_="nfl-c-matchup-strip__team-name").find(class_="nfl-c-matchup-strip__team-fullname").text
-            matchup_team_names = list(map(lambda name: find_team_name(name).strip(), matchup_teams_soup))
-            away_team = matchup_team_names[0]
-            home_team = matchup_team_names[1]
+            matchup_time = get_matchup_time(matchup)
+            away_team, home_team = get_matchup_teams(matchup)
             
-            matchup_time_details = matchup.find(class_ = "nfl-c-matchup-strip__date-time")
-
-            matchup_time = None
-
-            if (matchup_time_details != None):
-                matchup_time = matchup_time_details.text.strip()
-
             nfl_matchup = NFLMatchup(year, section_date, matchup_time, home_team, away_team)
+            
             nfl_matchup_list.append(nfl_matchup)
     
     return nfl_matchup_list
